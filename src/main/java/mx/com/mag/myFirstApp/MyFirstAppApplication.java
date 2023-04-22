bebf10fd418198b714ea0157eef63785a72c4a73
mmunoz29 <130855689+mmunoz29@users.noreply.github.com>
2023-04-21 23:55:21 -0600

    primer commit

---
+++ src/main/java/mx/com/mag/myFirstApp/MyFirstAppApplication.java
@@ -0,0 +1,13 @@
+package mx.com.mag.myFirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstAppApplication.class, args);
+	}
+
+}
